d6496af5165ad974f5b43a9cf995647c306f2065
GautamUIET <gautamjain09687@gmail.com>
2025-07-04 02:41:26 +0530

    firs

--- client/.next/static/chunks/src_app_layout_tsx_c0237562._.js
+++ client/.next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
-  "static/chunks/node_modules_d5c98cb6._.js",
+  "static/chunks/node_modules_93b27ffe._.js",
   "static/chunks/src_de3f5064._.js"
 ],
     source: "dynamic"
