8b610a7e04149801c2682b5caa33a3ff09bb560d
GautamUIET <gautamjain09687@gmail.com>
2025-07-04 15:40:14 +0530

    image demo

--- client/.next/static/chunks/src_de3f5064._.js
+++ client/.next/static/chunks/src_de3f5064._.js
@@ -517,7 +517,7 @@ const Sidebar = ()=>{
                     children: [
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                             className: "text-xl font-bold text-gray-800 dark:text-white",
-                            children: "EDLIST"
+                            children: "TaskPilot"
                         }, void 0, false, {
                             fileName: "[project]/src/app/(components)/Navbar/Sidebar/index.tsx",
                             lineNumber: 55,
@@ -563,7 +563,7 @@ const Sidebar = ()=>{
                             children: [
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("h3", {
                                     className: "text-md font-bold tracking-wide dark:text-gray-200",
-                                    children: "EDROH TEAM"
+                                    children: "Task Pilot"
                                 }, void 0, false, {
                                     fileName: "[project]/src/app/(components)/Navbar/Sidebar/index.tsx",
                                     lineNumber: 78,
